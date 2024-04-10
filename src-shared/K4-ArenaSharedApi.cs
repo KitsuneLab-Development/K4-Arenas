62248d0eab510c8f3081b9cbe96694cc5e50ab97
K4ryuu <104531589+K4ryuu@users.noreply.github.com>
2024-04-10 18:29:25 +0200

    Add Shared Api

---
+++ src-shared/K4-ArenaSharedApi.cs
@@ -0,0 +1,10 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace K4ArenaSharedApi
+{
+	public interface IK4ArenaSharedApi
+	{
+		public int AddSpecialRound(string name, int teamSize, Action<List<CCSPlayerController>?, List<CCSPlayerController>?> startFunction, Action<List<CCSPlayerController>?, List<CCSPlayerController>?> endFunction);
+		public void RemoveSpecialRound(int id);
+	}
+}
