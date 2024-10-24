09a728d27ccee2c0494db0069207509955415f06
K4ryuu <104531589+K4ryuu@users.noreply.github.com>
2024-10-24 19:38:57 +0200

    Update module version to 1.5.1 in PluginManifest.cs

--- src-plugin/Plugin/PluginManifest.cs
+++ src-plugin/Plugin/PluginManifest.cs
@@ -10,7 +10,7 @@ namespace K4Arenas
 
         public override string ModuleAuthor => "K4ryuu";
 
-        public override string ModuleVersion => "1.5.0 " +
+        public override string ModuleVersion => "1.5.1 " +
 #if RELEASE
             "(release)";
 #else
